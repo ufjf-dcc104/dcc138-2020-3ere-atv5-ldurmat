42f5123a5d9fe164f9183c00d8e696431f0a01c8
lowrran <76402654+ldurmat@users.noreply.github.com>
2021-03-12 04:03:42 -0300

    Adiciona exemplo de uso de audio

--- js/Main.js
+++ js/Main.js
@@ -7,6 +7,7 @@ assets.carregaImagem("garota", "assets/garota.png");
 assets.carregaImagem("esqueleto", "assets/skelly.png");
 assets.carregaImagem("orc", "assets/orc.png");
 assets.carregaAudio("moeda", "assets/coin.wav");
+assets.carregaAudio("boom", "assets/boom.wav");
 
 const canvas = document.querySelector("canvas");
 console.log(canvas);
@@ -27,5 +28,9 @@ document.addEventListener("keydown", (e) => {
       break;
     case "c":
       assets.audio("moeda").play();
+      break;
+    case "b":
+      assets.audio("boom").play();
+      break;
   }
 });
