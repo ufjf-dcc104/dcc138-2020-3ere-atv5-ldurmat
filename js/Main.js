81a5bcd4ad23a87b85d602761654cf3cb3383a85
lowrran <76402654+ldurmat@users.noreply.github.com>
2021-03-12 04:20:56 -0300

    Adiciona mixer.

--- js/Main.js
+++ js/Main.js
@@ -1,5 +1,6 @@
 import AssetManager from "./AssetManager.js";
 import Cena from "./Cena.js";
+import Mixer from "./Mixer.js";
 import Sprite from "./Sprite.js";
 
 const assets = new AssetManager();
@@ -9,6 +10,7 @@ assets.carregaImagem("orc", "assets/orc.png");
 assets.carregaAudio("moeda", "assets/coin.wav");
 assets.carregaAudio("boom", "assets/boom.wav");
 
+const mixer = new Mixer(10);
 const canvas = document.querySelector("canvas");
 console.log(canvas);
 const cena1 = new Cena(canvas, assets);
@@ -27,10 +29,10 @@ document.addEventListener("keydown", (e) => {
       cena1.parar();
       break;
     case "c":
-      assets.audio("moeda").play();
+      mixer.play(assets.audio("moeda"));
       break;
     case "b":
-      assets.audio("boom").play();
+      mixer.play(assets.audio("boom"));
       break;
   }
 });
