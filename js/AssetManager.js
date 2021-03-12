e806cd2039b7d79aacc27b65915b243388ad6bef
lowrran <76402654+ldurmat@users.noreply.github.com>
2021-03-11 22:00:44 -0300

    Adiciona log de img load e metodo acabou

--- js/AssetManager.js
+++ js/AssetManager.js
@@ -7,8 +7,13 @@ export default class AssetManager {
 
   carregaImagem(chave, source) {
     const img1 = new Image();
+    img1.addEventListener("load", ()=>{
+        console.log(`Imagem ${this.carregadas+1}/${this.aCarregar} carregada!`);
+        this.carregadas++;
+    })
     img1.src = source;
     this.imagens.set(chave, img1);
+    this.aCarregar++;
   }
 
   img(chave) {
@@ -20,5 +25,9 @@ export default class AssetManager {
       return `${((this.carregadas / this.aCarregar) * 100).toFixed(2)}%`;
     }
     return "Nada a carregar";
+  }
+
+  acabou(){
+      return this.carregadas === this.aCarregar;
   }
 }
